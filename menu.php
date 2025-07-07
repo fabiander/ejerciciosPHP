5a68875de5266361dda5f91391bb117f28d04a01
Erwin Fabian Ortega <erwinfabian0222@hotmail.com>
2025-07-07 01:38:35 -0500

    first commit

---
+++ menu.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Menú de Ejercicios PHP</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Menú de Ejercicios</h1>
+        <div class="menu">
+            <a class="boton" href="edad.php">Calcular Edad</a>
+            <a class="boton" href="notas.php">Formulario de Datos</a>
+            <a class="boton" href="salario.php">Nombre Aleatorio</a>
+            <a class="boton" href="area.php">Ádivinanza</a>
+            <a class="boton" href="impuestos.php">Cálculo de Impuestos</a>
+            <a class="boton" href="temperatura.php">Listado de peliculas</a>
+            <a class="boton" href="peso.php">Notas de alumnos</a>
+            <a class="boton" href="factura.php">Validador de victimas</a>
+            <a class="boton" href="tabla.php">Formulario padre</a>
+            <a class="boton" href="numero.php">Calcular Precio</a>
+        </div>
+    </div>
+</body>
+
+</html>
